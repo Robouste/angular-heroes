e086a67cd15cdb7da6a89f7ff54b70db472ac877
Robouste <robindenis88@gmail.com>
2016-10-28 09:45:24 +0200

    initial commit

---
+++ angularQuickStart/app/app.component.ts
@@ -0,0 +1,34 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+	selector: 'my-app',
+	templateUrl: 'templates/list.html',
+	styleUrls: ['styles/list.css']
+})
+
+export class AppComponent {
+	title = 'Tour of Heroes';
+	hero: Hero = {
+		id: 1,
+		name: 'Fatman'
+	};
+	heroes = HEROES;
+}
+
+export class Hero {
+	id: number;
+	name: string;
+}
+
+const HEROES: Hero[] = [
+	{ id: 11, name: 'Fatman' },
+	{ id: 12, name: 'Slipman' },
+	{ id: 13, name: 'Scatwoman' },
+	{ id: 14, name: 'Celeri' },
+	{ id: 15, name: 'Magneta' },
+	{ id: 16, name: 'RubberMan' },
+	{ id: 17, name: 'Dynama' },
+	{ id: 18, name: 'Dr IQ' },
+	{ id: 19, name: 'Magma' },
+	{ id: 20, name: 'Tornado' }
+];
